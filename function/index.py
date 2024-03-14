2fc144d1b590d3d1caddf557ec2e01cf154d4702
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2024-03-14 15:01:57 +0900

    Bedrock関連リソースを修正

--- function/index.py
+++ function/index.py
@@ -1,17 +1,20 @@
-import boto3
 import json
-import os
 import logging
+import os
+from typing import Any, Dict
+
+import boto3
 
 logger = logging.getLogger()
 
-kendra_client = boto3.client('kendra')
-bedrock_runtime_client = boto3.client('bedrock-runtime')
+kendra_client = boto3.client("kendra")
+bedrock_runtime_client = boto3.client(
+    service_name="bedrock-runtime", region_name=os.getenv("BEDROCK_REGION")
+)
 
 
 # Kendra から検索結果を取得
-def get_retrieval_result(query_text, index_id):
-
+def get_retrieval_result(query_text: Any | None, index_id: str | None) -> str:
     response = kendra_client.query(
         QueryText=query_text,
         IndexId=index_id,
@@ -24,11 +27,13 @@ def get_retrieval_result(query_text, index_id):
     )
 
     # Kendra の応答から最初の5つの結果を抽出
-    results = response['ResultItems'][:5] if response['ResultItems'] else []
+    results = response["ResultItems"][:5] if response["ResultItems"] else []
 
     # 結果からドキュメントの抜粋部分のテキストを抽出
     for i in range(len(results)):
-        results[i] = results[i].get("DocumentExcerpt", {}).get("Text", "").replace('\\n', ' ')
+        results[i] = (
+            results[i].get("DocumentExcerpt", {}).get("Text", "").replace("\\n", " ")
+        )
 
     print("Received results:" + json.dumps(results, ensure_ascii=False))
 
@@ -36,9 +41,9 @@ def get_retrieval_result(query_text, index_id):
 
 
 # Lambda のハンドラー関数
-def lambda_handler(event, context):
-    user_prompt = event.get('user_prompt')
-    index_id = os.getenv('KENDRA_INDEX_ID')
+def lambda_handler(event: Dict[Any, Any], context: Any) -> Any:
+    user_prompt = event.get("user_prompt")
+    index_id = os.getenv("KENDRA_INDEX_ID")
 
     prompt = f"""\n\nHuman:
     [参考]情報をもとに[質問]に適切に答えてください。
@@ -50,24 +55,23 @@ def lambda_handler(event, context):
     """
 
     # 各種パラメーターの指定
-    modelId = os.getenv('BEDROCK_MODEL_ID')
-    accept = 'application/json'
-    contentType = 'application/json'
+    modelId = os.getenv("BEDROCK_MODEL_ID")
+    accept = "application/json"
+    contentType = "application/json"
 
-    body = json.dumps({
-        "prompt": prompt,
-        "max_tokens_to_sample": 600,
-    })
-
-    response = bedrock_runtime_client.invoke_model(
-        modelId=modelId,
-        accept=accept,
-        contentType=contentType,
-        body=body
+    body = json.dumps(
+        {
+            "prompt": prompt,
+            "max_tokens_to_sample": 600,
+        }
     )
 
-    response_body = json.loads(response.get('body').read())
+    response = bedrock_runtime_client.invoke_model(
+        modelId=modelId, accept=accept, contentType=contentType, body=body
+    )
+
+    response_body = json.loads(response.get("body").read())
 
     print("Received response_body:" + json.dumps(response_body, ensure_ascii=False))
 
-    return response_body.get('completion')
+    return response_body.get("completion")
